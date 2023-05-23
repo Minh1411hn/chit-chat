c870ca1782300a3dd7bff19ec8bd5243901d42f4
Duc Minh <Minh1411hn@Gmail.com>
2023-05-23 14:18:44 +0700

    fix again

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: ["https://chat.minhtran.live:5173"],
+    origin: ["https://chat.minhtran.live"],
 }));
 app.set("trust proxy", 1);
 
