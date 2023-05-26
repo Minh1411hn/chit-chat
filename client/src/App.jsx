61fa297a8fd54d217d8f718540d785c4a23dfdc9
Duc Minh <Minh1411hn@Gmail.com>
2023-05-26 22:29:46 +0700

    good

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+  axios.defaults.baseURL = 'https://chat-api-8me2w.ondigitalocean.app/api';
   axios.defaults.withCredentials = true;
   return (
       <UserContextProvider>
