77c6f475ddb869989bb46458e1313c61274e23f7
Duc Minh <Minh1411hn@Gmail.com>
2023-05-26 22:47:24 +0700

    fix /api/api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'https://chat-api-8me2w.ondigitalocean.app/api';
+  axios.defaults.baseURL = 'https://chat.minhtran.live';
   axios.defaults.withCredentials = true;
   return (
       <UserContextProvider>
