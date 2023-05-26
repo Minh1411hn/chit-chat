7da294d22cd543b7cbbb1d88800e0e98d7d0fb09
Duc Minh <Minh1411hn@Gmail.com>
2023-05-27 00:13:51 +0700

    fix ws to localhost

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -20,7 +20,7 @@ export default function Chat() {
         connectToWs();
     }, [selectedUserId]);
     function connectToWs() {
-        const ws = new WebSocket('ws://chat.minhtran.live');
+        const ws = new WebSocket('ws://localhost:4040');
         setWs(ws);
         ws.addEventListener('message', handleMessage);
         ws.addEventListener('close', () => {
