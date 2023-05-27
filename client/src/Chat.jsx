b0b91a22802dec3e0a797d24eda20866af0f3478
Duc Minh <Minh1411hn@Gmail.com>
2023-05-27 08:57:31 +0700

    change ws to cht.minhtran.live:4040

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -26,6 +26,7 @@ export default function Chat() {
         ws.addEventListener('close', () => {
             setTimeout(() => {
                 console.log('Disconnected. Trying to reconnect.');
+                console.log(import.meta.env.VITE_API_BASE_URL);
                 connectToWs();
             }, 1000);
         });
