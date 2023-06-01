2ef0fbdee22083e114014268018267540a202506
Duc Minh <Minh1411hn@Gmail.com>
2023-06-01 16:46:55 +0700

    trying

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -148,7 +148,7 @@ export default function Chat() {
         <div className="flex h-screen overflow-hidden">
             <div className={`bg-white flex flex-col ${isMobile? (selectedUserId ? "hidden" : "w-full") : "w-1/3" } md:flex`}>
                 {/*LOGO*/}
-                <div className="text-[#ED7A46] font-bold text-lg flex gap-2 p-4 pb-10">
+                <div className="sticky top-0 text-[#ED7A46] font-bold text-lg flex gap-2 p-4 pb-10">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
                     </svg>
@@ -187,7 +187,7 @@ export default function Chat() {
                 </div>
 
                 {/* my profile section */}
-                <div className=" bottom-0 bg-gray-100 h-10 rounded-full m-5 mb-2 p-2 text-center flex items-center justify-center">
+                <div className="sticky bottom-0 bg-gray-100 h-10 rounded-full m-5 mb-2 p-2 text-center flex items-center justify-center">
                     <span className=" mr-2 text-md text-grey-600 flex items-center">
                         <Gravatar email={email} default="retro" className="text-center w-8 h-8 mr-2 object-cover rounded-full"/>
                         {username}
@@ -201,7 +201,7 @@ export default function Chat() {
             <div className={`bg-[#FDF3E5] flex flex-col h-full  ${ isMobile ? (selectedUserId ? "w-full" : "hidden") : "w-2/3"}`}>
                 {!!selectedUserId && (
                     // Chatting user info
-                    <div className={`flex items-center border-b-[1px] border-[#EFE6D8] pl-5 py-5 ${isMobile ? "sticky" : ""}`}>
+                    <div className={`flex items-center border-b-[1px] border-[#EFE6D8] pl-5 py-5 ${isMobile ? "sticky top-0" : ""}`}>
                         {/* Mobile Toggle Button */}
                         { isMobile && selectedUserId && (
                             <button
@@ -273,7 +273,7 @@ export default function Chat() {
                     )}
                 </div>
                 {!!selectedUserId && (
-                    <form className={`flex gap-2 p-2 ${isMobile? "sticky" : ""}`} onSubmit={sendMessage}>
+                    <form className={`flex gap-2 p-2 ${isMobile? "sticky bottom-0" : ""}`} onSubmit={sendMessage}>
                         <input type="text"
                                value={newMessageText}
                                onChange={ev => setNewMessageText(ev.target.value)}
